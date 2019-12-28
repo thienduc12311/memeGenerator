c56a441b65052ba2b3562b3847584afb8e05b193
thienduc12311 <59289834+thienduc12311@users.noreply.github.com>
2019-12-29 00:41:50 +0700

    demo1

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+
+import React from "react"
+
+
+function Header(props) {
+    return (
+        <header>
+            <img src={"https://img1.kienthucvui.vn/uploads/2019/09/16/mat-troll-cuoi-dep_031406200.jpg"} alt="Problem?"/>
+            <p>Meme Generator</p>
+        </header>
+    )
+}
+
+export default Header
